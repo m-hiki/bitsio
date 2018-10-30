3fd91fd39f9fff9033a3c3599ea6ca9ea02f4538
Minoru Hiki <minoruhiki@gmail.com>
2018-10-30 19:50:17 +0900

    #1 Anyway , add write method in msb first. And refactor.

--- bitsio/bitsio.py
+++ bitsio/bitsio.py
@@ -25,8 +25,8 @@ class BitsIO(object):
         self.bitorder = bitorder
         self.byteorder = bitorder  # Byte order must be equal to bit order
         self.bitbuf_size = DEFAULT_BUFSIZE
-        self._init_wbitbuf()
-        self._init_rbitbuf()
+        self._init_wbuf()
+        self._init_rbuf()
         self.get_shift = {
             MSB: lambda left: left,
             LSB: lambda left: DEFAULT_BUFSIZE - left - 1
@@ -39,30 +39,56 @@ class BitsIO(object):
     def read1(self):
         self.rleft -= 1
         shift = self.get_shift[self.bitorder](self.rleft)
-        bit = (self.rbitbuf >> shift) & 1
+        bit = (self.rbuf >> shift) & 1
         if self.rleft == 0:
-            self._init_rbitbuf()
+            self._init_rbuf()
         return bit
 
-    def _init_rbitbuf(self):
-        self.rbitbuf = int.from_bytes(self.bytesio.read(
+    def _init_rbuf(self):
+        self.rbuf = int.from_bytes(self.bytesio.read(
             BYTES_LENGTH), byteorder=self.byteorder)
         self.rleft = self.bitbuf_size
-
-    def write(self, bits: int, size: int):
-        ...
 
     def write1(self, bit: int):
         self.wleft -= 1
         shift = self.get_shift[self.bitorder](self.wleft)
-        self.wbitbuf |= (bit & 1) << shift
+        self.wbuf |= (bit & 1) << shift
 
         if self.wleft == 0:
-            bytes = self.wbitbuf.to_bytes(
-                length=BYTES_LENGTH, byteorder=self.byteorder)
-            self.bytesio.write(bytes)
-            self._init_wbitbuf()
+            self._flush()
 
-    def _init_wbitbuf(self):
-        self.wbitbuf = 0
+    def write(self, bits: int, size: int):
+        l = size - self.wleft
+        bits &= (1 << size) - 1
+        # print('{:08b}'.format(bits))
+
+        # msb first
+        if l < 0:
+            shift = -l
+            self.wbuf |= bits << shift
+            self.wleft = shift
+        else:
+            self.wbuf |= bits >> l
+            self._flush()
+            self.wleft = self.bitbuf_size - l
+
+            if self.wleft < self.bitbuf_size:
+                self.wbuf = bits << self.wleft
+
+    def write_align(self):  # flash
+        ...
+
+    def _flush(self):
+        bytes = self.wbuf.to_bytes(
+            length=BYTES_LENGTH, byteorder=self.byteorder)
+        #print('wbuf: {:032b}'.format(self.wbuf))
+        #print('bytes:   {0}'.format(bytes))
+        self.bytesio.write(bytes)
+        self._init_wbuf()
+
+    def _init_wbuf(self):
+        self.wbuf = 0
         self.wleft = self.bitbuf_size
+
+    def getvalue(self):
+        return self.bytesio.getvalue()
--- tests/test_bitsio.py
+++ tests/test_bitsio.py
@@ -4,7 +4,8 @@ from bitsio import BitsIO
 
 class TestBitsIO(unittest.TestCase):
     def setUp(self):
-        self.WRITE_TEST_INPUT = bytearray([1, 2, 3, 4, 5, 6, 7, 8])
+        self.WRITE1_TEST_INPUT = bytearray([1, 2, 3, 4, 5, 6, 7, 8])
+        #t = 0b0001_0010_0011_0100_0101_0110
 
     def test_init(self):
         exception = None
@@ -18,25 +19,36 @@ class TestBitsIO(unittest.TestCase):
 
     def test_write1_msb_first(self):
         bitsio = BitsIO(b'',  bitorder='big')
-        for byte in self.WRITE_TEST_INPUT:
+        for byte in self.WRITE1_TEST_INPUT:
             for n in range(0, 8):
                 shift = 7 - n
                 bit = (byte >> shift) & 1
                 bitsio.write1(bit)
 
-        actual_bytebuf = bitsio.bytesio.getvalue()
-        self.assertEqual(self.WRITE_TEST_INPUT, actual_bytebuf)
+        actual_bytebuf = bitsio.getvalue()
+        self.assertEqual(self.WRITE1_TEST_INPUT, actual_bytebuf)
 
     def test_write1_lsb_first(self):
         bitsio = BitsIO(b'', bitorder='little')
-        for byte in self.WRITE_TEST_INPUT:
+        for byte in self.WRITE1_TEST_INPUT:
             for n in range(0, 8):
                 shift = n
                 bit = (byte >> shift) & 1
                 bitsio.write1(bit)
 
-        actual_bytebuf = bitsio.bytesio.getvalue()
-        self.assertEqual(self.WRITE_TEST_INPUT, actual_bytebuf)
+        actual_bytebuf = bitsio.getvalue()
+        self.assertEqual(self.WRITE1_TEST_INPUT, actual_bytebuf)
+
+    def test_write_msb_first(self):
+        bitsio = BitsIO(b'',  bitorder='big')
+        bitsio.write(0x01, 8)
+        bitsio.write(0x02, 8)
+        bitsio.write(0x03, 8)
+        bitsio.write(0x04, 8)
+        print(bitsio.getvalue())
+
+    def test_write_lsb_first(self):
+        bitsio = BitsIO(b'', bitorder='little')
 
 
 if __name__ == '__main__':
