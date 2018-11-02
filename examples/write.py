cfc6b9aec6e399877448c2d5280dbe2ebc64cc3b
Minoru Hiki <minoruhiki@gmail.com>
2018-11-02 20:20:24 +0900

    #1 Add write example

--- examples/write.py
+++ examples/write.py
@@ -3,7 +3,13 @@ from bitsio import BitsIO
 
 
 def main():
-    ...
+    test_input = 'TEST'
+    bitsio = BitsIO(b'', bitorder='big')
+    for s in test_input:
+        bitsio.write(ord(s), 8)
+
+    with open('test.txt', 'wb') as f:
+        f.write(bitsio.getvalue())
 
 
 if __name__ == '__main__':
