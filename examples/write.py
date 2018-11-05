c3a0d1ee34f8444abd889dbbeaa497a9c1d41da4
Minoru Hiki <minoruhiki@gmail.com>
2018-11-05 14:25:18 +0900

    #1 Change specification. The IO class is removed from BitsIO class. Any class can be used for I/O.

--- examples/write.py
+++ examples/write.py
@@ -4,12 +4,11 @@ from bitsio import BitsIO
 
 def main():
     test_input = 'TEST'
-    bitsio = BitsIO(b'', bitorder='big')
-    for s in test_input:
-        bitsio.write(ord(s), 8)
 
     with open('test.txt', 'wb') as f:
-        f.write(bitsio.getvalue())
+        bitsio = BitsIO(f, bitorder='big')
+        for s in test_input:
+            bitsio.write(ord(s), 8)
 
 
 if __name__ == '__main__':
